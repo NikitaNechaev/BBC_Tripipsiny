a58e028f8ea8e403fa082d3d91bb519ae4ecb6a7
NikitaNechaev <2805niki05ta@gmail.com>
2022-06-21 20:19:11 +0300

    Add file output (MatrixComparisonModAlg)
    Добавлена возможность вывода координат островков в текстовый файл

--- MatrixComparisonModAlg.py
+++ MatrixComparisonModAlg.py
@@ -3,13 +3,14 @@ from tqdm import tqdm
 import numpy as np, matplotlib.pyplot as plt
 
 filename = 'SourceFiles\generated_sequence.fa'
+f = open('res.txt', 'w')
 
 seq = SeqIO.parse(filename, 'fasta')
 for rec in seq:
     example = rec
     break
 
-CPG_SIZE = 300
+CPG_SIZE = 200
 NON_CPG = np.array([[0.300, 0.205, 0.285, 0.210], [0.322, 0.298, 0.078, 0.302], [0.248, 0.246, 0.298, 0.208], [0.177, 0.239, 0.292, 0.292]])
 CPG = np.array([[0.180, 0.274, 0.426, 0.120], [0.171, 0.368, 0.274, 0.188], [0.161, 0.339, 0.375, 0.125], [0.079, 0.355, 0.384, 0.182]])
 #целевые матрицы (МЦ)
@@ -32,8 +33,13 @@ comparison = np.full((4,4), 0.0)
 comparison_ncpg = np.full((4,4), 0.0)
 
 x = np.linspace(0, len(example.seq), len(example.seq)) 
-yCpG = []
-yNon = []
+yCpG = [0]
+yNon = [0]
+islands_list = []
+res = [[]]
+
+def ScanerIncrease (arr):
+    print()
 
 def MatrixInit (def_matrix, iter_num:int):
     for j in range(CPG_SIZE): #заполнение матрицы CpG первыми 300 нукл.
@@ -68,17 +74,12 @@ for i in tqdm(range(CPG_SIZE, len(example.seq)-1), leave=True): #scaner move (Cp
         for l in range(4):
             comparison[k][l] = (CPG[k][l] - (dict_nuc_cpg[nucList[k]][nucList[l]] / sum_of_row))**2
             comparison_ncpg[k][l] = (NON_CPG[k][l] - (dict_nuc_noncpg[nucList[k]][nucList[l]] / sum_of_row_ncgp))**2
-    yCpG.append(-np.sum(comparison))
-    yNon.append(-np.sum(comparison_ncpg))
-
-dyn_row = []
-dyn_row_coords = []
-islands_list = []
-
-for i in range(len(yCpG)-1):
-    while yCpG[i] > yNon[i]:
-        dyn_row.append(yCpG[i])
-        i+=1
+    yCpG.append(np.sum(comparison))
+    yNon.append(np.sum(comparison_ncpg))
+    if yCpG[i-1-CPG_SIZE] > yNon[i-1-CPG_SIZE]:
+        islands_list.append(i-1-CPG_SIZE)
+    elif yCpG[i-1-CPG_SIZE] > yNon[i-1-CPG_SIZE] and yCpG[i-CPG_SIZE] < yNon[i-CPG_SIZE]:
+        f.write(islands_list[int(CPG_SIZE/2)], '\t', islands_list[:(int(CPG_SIZE/2))], '\n')
 
 fig, ax = plt.subplots()
 ax.plot(yCpG)
