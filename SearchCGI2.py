b8660e8a9ba680b32db63c45cada1cb080553492
NikitaNechaev <2805niki05ta@gmail.com>
2022-07-02 21:42:59 +0300

    SSH test commit

--- SearchCGI2.py
+++ SearchCGI2.py
@@ -2,6 +2,7 @@ import math
 from Bio import SeqIO
 from tqdm import tqdm
 import numpy as np, matplotlib.pyplot as plt
+#test commit
 
 filename = 'SourceFiles\hg38_chr1_and_chr2.fa'
 f = open('res.txt', 'w')
